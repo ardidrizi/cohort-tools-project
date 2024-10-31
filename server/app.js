b7d149884772e2bf9851639e2c7657e2775501df
Ardian Idrizi <ardianidizi@gmail.com>
2024-10-31 16:33:04 +0100

    Added routes for Cohorts and Students

--- server/app.js
+++ server/app.js
@@ -33,6 +33,7 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+// Retrives all cohorts in the database collection
 app.get("/api/cohorts/", (req, res) => {
   try {
     Cohort.find().then((cohorts) => {
@@ -43,6 +44,64 @@ app.get("/api/cohorts/", (req, res) => {
   }
 });
 
+// Creates a new cohort in the database collection
+app.post("/api/cohorts", async (req, res) => {
+  try {
+    await Cohort.create(req.body).then((cohort) => {
+      console.log(cohort);
+      res.status(201).json(cohort);
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error creating cohort", error });
+  }
+});
+
+// Retrieves a specific cohort by id
+app.get("/api/cohorts/:cohortId", async (req, res) => {
+  const { cohortId } = req.params;
+
+  try {
+    await Cohort.findById({ _id: cohortId }).then((cohort) => {
+      res.status(200).json(cohort);
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error fetching cohort", error });
+  }
+});
+
+// Updates a specific cohort by id
+app.put("/api/cohorts/:cohortId", async (req, res) => {
+  const { cohortId } = req.params;
+
+  try {
+    await Cohort.findByIdAndUpdate({ _id: cohortId }, req.body).then(
+      (cohort) => {
+        res.status(200).json(cohort);
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error updating cohort", error });
+  }
+});
+
+// Deletes a specific cohort by id
+app.delete("/api/cohorts/:cohortId", async (req, res) => {
+  const { cohortId } = req.params;
+
+  try {
+    await Cohort.findByIdAndDelete({ _id: cohortId }).then(() => {
+      res.status(204).json();
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error deleting cohort", error });
+  }
+});
+
+// Retrives all students in the database collection
 app.get("/api/students", (req, res) => {
   try {
     Student.find().then((students) => {
@@ -50,6 +109,75 @@ app.get("/api/students", (req, res) => {
     });
   } catch (error) {
     res.status(500).json({ message: "Error fetching students", error });
+  }
+});
+
+// Creates a new cohort in the database collection
+app.post("/api/students", async (req, res) => {
+  try {
+    await Student.create(req.body).then((student) => {
+      res.status(201).json(student);
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error creating student", error });
+  }
+});
+
+// Retrieves all of the students for a given cohort
+app.get("/api/students/cohort/:cohortId", async (req, res) => {
+  const { cohortId } = req.params;
+  console.log(cohortId);
+
+  try {
+    await Student.find({ cohort: cohortId }).then((students) => {
+      res.status(200).json(students);
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching students", error });
+  }
+});
+
+// Retrieves a specific student by id
+app.get("/api/students/:studentId", async (req, res) => {
+  const { studentId } = req.params;
+
+  try {
+    await Student.findById({ _id: studentId }).then((student) => {
+      res.status(200).json(student);
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching student", error });
+  }
+});
+
+// Updates a specific student by id
+app.put("/api/students/:studentId", async (req, res) => {
+  const { studentId } = req.params;
+
+  try {
+    await Student.findByIdAndUpdate({ _id: studentId }, req.body).then(
+      (student) => {
+        res.status(200).json(student);
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error updating student", error });
+  }
+});
+
+// Deletes a specific student by id
+app.delete("/api/students/:studentId", async (req, res) => {
+  const { studentId } = req.params;
+
+  try {
+    await Student.findByIdAndDelete({ _id: studentId }).then(() => {
+      res.status(204).json();
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error deleting student", error });
   }
 });
 
